a1f4a88f6ea14fae970b2d976edafcad00ac51e4
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-09-26 21:36:48 +0530

    Imported Db credentials

--- mb-sites-backend/helper/database.py
+++ mb-sites-backend/helper/database.py
@@ -6,3 +6,9 @@ from dotenv import load_dotenv
 
 #Loadin the env variables
 load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_NAME = os.getenv("DB_NAME", "meeting_db")
