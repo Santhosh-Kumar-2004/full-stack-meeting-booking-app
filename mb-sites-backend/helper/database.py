0b855cee0b9235d0fd3615ab33b97476bf4ce261
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-09-26 21:36:20 +0530

    Loaded env file content

--- mb-sites-backend/helper/database.py
+++ mb-sites-backend/helper/database.py
@@ -3,3 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+
+#Loadin the env variables
+load_dotenv()
