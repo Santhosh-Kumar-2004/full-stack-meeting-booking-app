10c930eeefecdba70f64078f9b7cea8f0173fec8
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-09-26 19:52:42 +0530

    Created users table

--- mb-sites-database/schema.sql
+++ mb-sites-database/schema.sql
@@ -1,3 +1,11 @@
 CREATE DATABASE meeting_app_db;
 
 USE meeting_app_db;
+
+CREATE TABLE users (
+  id CHAR(36) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
