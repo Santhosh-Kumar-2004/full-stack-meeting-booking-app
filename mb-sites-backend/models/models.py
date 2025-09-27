9c80f6d58362f66a0cc6fabda10d047cb5427b75
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-09-27 20:40:14 +0530

    Created models.py file

---
+++ mb-sites-backend/models/models.py
@@ -0,0 +1,31 @@
+# backend/models/models.py
+
+from sqlalchemy import Column, String, Enum, DateTime, ForeignKey
+from sqlalchemy.dialects.mysql import CHAR
+from sqlalchemy.orm import relationship
+from datetime import datetime
+import uuid
+
+from helper.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(CHAR(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    role = Column(Enum("user", "admin"), default="user")
+
+
+
+class Booking(Base):
+    __tablename__ = "bookings"
+
+    id = Column(CHAR(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    guest_name = Column(String(100), nullable=False)
+    guest_email = Column(String(100), nullable=False)
+    reason = Column(String(255), nullable=False)
+    meeting_time = Column(DateTime, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
