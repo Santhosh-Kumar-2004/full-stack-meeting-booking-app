8f7c7214ea4d25717d769fc325ab3e7304e3a032
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-09-27 20:55:14 +0530

    Added basic tempate for booking schema

---
+++ mb-sites-backend/schemas/booking.py
@@ -0,0 +1,25 @@
+# backend/schemas/booking.py
+
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import Optional
+
+# For creating booking (input)
+class BookingCreate(BaseModel):
+    guest_name: str
+    guest_email: EmailStr
+    reason: str
+    meeting_time: datetime
+
+# For showing booking (output)
+class BookingResponse(BaseModel):
+    id: str
+    guest_name: str
+    guest_email: EmailStr
+    reason: str
+    meeting_time: datetime
+    created_at: datetime
+    user_id: str
+
+    class Config:
+        from_attributes = True
