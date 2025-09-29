27fd7d20f9d21d9a72903867b2950cfa115ec27e
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-09-29 20:00:54 +0530

    Commented the unneccassary

--- mb-sites-backend/routers/users.py
+++ mb-sites-backend/routers/users.py
@@ -11,7 +11,7 @@ router = APIRouter(prefix="/auth", tags=["Users"])
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-MAX_PASSWORD_LENGTH = 72  # bcrypt limit
+# MAX_PASSWORD_LENGTH = 72  # bcrypt limit
 
 # Register User - 1
 @router.post("/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
@@ -40,11 +40,11 @@ def register_user(
             )
         
         # check password length
-        if len(user.password) > MAX_PASSWORD_LENGTH:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail=f"Password cannot exceed {MAX_PASSWORD_LENGTH} characters."
-            )
+        # if len(user.password) > MAX_PASSWORD_LENGTH:
+        #     raise HTTPException(
+        #         status_code=status.HTTP_400_BAD_REQUEST,
+        #         detail=f"Password cannot exceed {MAX_PASSWORD_LENGTH} characters."
+        #     )
         
         # hash password
         hashed_password = hash_password(user.password)
