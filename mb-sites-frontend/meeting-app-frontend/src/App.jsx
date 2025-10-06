5d59c7bd1bba6c94bba178559b73aea75aeef03f
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-10-06 19:41:20 +0530

    imported Home and Navbar

--- mb-sites-frontend/meeting-app-frontend/src/App.jsx
+++ mb-sites-frontend/meeting-app-frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-import Register from './pages/Register'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Register from './pages/Register'
 
 function App() {
 
