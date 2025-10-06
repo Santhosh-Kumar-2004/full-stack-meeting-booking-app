3e4ce0b9f3ea78099dd7595c0d637ce1101491f4
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-10-06 19:57:02 +0530

    Imported react toatify

--- mb-sites-frontend/meeting-app-frontend/src/App.jsx
+++ mb-sites-frontend/meeting-app-frontend/src/App.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from './pages/Register'
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
 
   return (
