0854fdc8888c49f0020ca93a350f672f12cd0699
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-10-06 18:54:59 +0530

    ed register page onto app

--- mb-sites-frontend/meeting-app-frontend/src/App.jsx
+++ mb-sites-frontend/meeting-app-frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import Register from './pages/Register'
 
 function App() {
 
   return (
     <>
       {/* <h1>Hello world</h1> */}
+      <Register />
     </>
   )
 }
