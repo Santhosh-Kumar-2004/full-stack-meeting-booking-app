21d9d36c6ec6584bcbf20c32a26f981b12ee3c28
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-10-06 19:44:31 +0530

    Navbar error found

--- mb-sites-frontend/meeting-app-frontend/src/App.jsx
+++ mb-sites-frontend/meeting-app-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Register from './pages/Register'
 
