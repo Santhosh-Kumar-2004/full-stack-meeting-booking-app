b81ba5e0e27d1d9fc2713f7a328807a1fb509a56
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-10-06 19:40:24 +0530

    Added basic template for the Home file

---
+++ mb-sites-frontend/meeting-app-frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../index.css";
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <h1>Welcome to the Meeting Booking App</h1>
+      <p>Book, manage, and track your meetings effortlessly.</p>
+    </div>
+  );
+};
+
+export default Home;
