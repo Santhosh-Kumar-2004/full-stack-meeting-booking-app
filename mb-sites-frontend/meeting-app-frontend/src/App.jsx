60d81bc478e34104bf81030e46ae54f956355ca1
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-10-08 14:42:19 +0530

    imported the all users at App.jsx

--- mb-sites-frontend/meeting-app-frontend/src/App.jsx
+++ mb-sites-frontend/meeting-app-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from './pages/Register'
 import Login from './pages/Login';
+import AllUsers from './pages/AllUsers';
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -22,6 +23,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/admin/users" element={<AllUsers />} />
         </Routes>
       </Router>
 
